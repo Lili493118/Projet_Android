8c96923e049af464bcfd85e267b3d41e1391238c
Lili493118 <aoustinalyson@gmail.com>
2022-12-09 13:49:26 +0100

    add search bar + recup mot de la search bar

--- app/src/main/java/com/example/projet_aoustin/Fragment_Recherche.java
+++ app/src/main/java/com/example/projet_aoustin/Fragment_Recherche.java
@@ -5,17 +5,36 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 public class Fragment_Recherche extends Fragment {
     public Fragment_Recherche(){
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_recherche_layout, container, false);
         Log.d("Fragment Recherche","onCreateView");
+
+        Button searchbutton = rootView.findViewById(R.id.searchbutton);
+        searchbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText InputSearchText = rootView.findViewById(R.id.textinputbar);
+                String editTextValue = InputSearchText.getText().toString();
+                Log.d("RECHERCHE",editTextValue);
+            }
+        });
+
+
+
         return rootView;
     }
+
+
 }
