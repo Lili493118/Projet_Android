2b5766c7b272fd20888d63f26fbbf9a75ae00320
Lili493118 <aoustinalyson@gmail.com>
2022-12-21 17:34:35 +0100

    creation d'un theme

--- app/src/main/java/com/example/projet_aoustin/MainActivity.java
+++ app/src/main/java/com/example/projet_aoustin/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
