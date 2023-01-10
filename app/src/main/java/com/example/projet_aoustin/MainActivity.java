0310da4f3ce9671cd7cfccb41f979b033a3ac336
Lili493118 <aoustinalyson@gmail.com>
2023-01-10 16:16:11 +0100

    MainActivity supression warning

--- app/src/main/java/com/example/projet_aoustin/MainActivity.java
+++ app/src/main/java/com/example/projet_aoustin/MainActivity.java
@@ -6,12 +6,14 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.preference.PreferenceManager;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -27,6 +29,8 @@ import java.util.ArrayList;
  * Activité principale du projet
  */
 public class MainActivity extends AppCompatActivity {
+    /** TAG servant au log  */
+    private static final String TAG = "Test";
     /** Préference partagée de l'application  */
     SharedPreferences prefs;
     /** Chaine de charactère contenant le nom du dossier où sont stokées les images en cas d'enrengistrement */
@@ -43,84 +47,86 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        /** Assignation des valeurs aux attributs
+        /* Assignation des valeurs aux attributs
          * Definition des préférérence partagées à travers le Preference Manager
          * Définition de l'emplacement de stockage des images à partir des préférences
          * */
         prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        dossierDeStockage = new String(prefs.getString("emplacement",""));
+        dossierDeStockage = prefs.getString("emplacement","");
 
-        /** Définition du themes à appliquer à partir des préférences */
+        /* Définition du themes à appliquer à partir des préférences */
         GetThemeFromSharedPreference();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /** Récupration, Personalisation et Affichage de la toolbar*/
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        /* Récupration, Personalisation et Affichage de la toolbar*/
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
         myToolbar.setTitleTextColor(getResources().getColor(color.lightbeige));
         setSupportActionBar(myToolbar);
 
-        //Creation du Fragment manager
-        /** Création du Fragment Manager permettant la gestion des fragments */
+        /* Création du Fragment Manager permettant la gestion des fragments */
         FragmentManager monManager = getSupportFragmentManager();
 
-        /** Initialisation : réalisation de la première transaction affichant le fragment de recherche*/
+        /* Initialisation : réalisation de la première transaction affichant le fragment de recherche*/
         FragmentTransaction maTransaction = monManager.beginTransaction();
         maTransaction.add(R.id.fragment_container, new Fragment_Recherche(),null).commit();
 
-        /** Définit les actions à réaliser au moment du changement des préférences */
-        prefs.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
-            @Override
-            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-                switch (key){
-                    case "theme":
-                        /**
-                         * Change le thème
-                         * Alerte l'utlisateur qu'un redémrrage sera peut-etre necessaire
-                         * Re-créaction de l'activité afin de changer le thème */
-                        GetThemeFromSharedPreference();
-                        Toast.makeText(getApplicationContext(), "Si le thème ne change pas, redémarrer l'application", Toast.LENGTH_LONG).show();
-                        recreate();
-                        break;
-                    case "telecharger":
-                        /** Si l'enrengistrement devient autorisé lors du changement */
-                        /**
-                         * Alerte l'utlisateur qu'une permission supplémentaire (MANAGE_ALL_FILES) est requise
-                         * Commence une nouvelle activité permettant à l'utlisateur d'autoriser l'enrengistrement des images
-                         * */
-                        if((boolean) sharedPreferences.getAll().get(key)){
-                            if (Build.VERSION.SDK_INT >= 30){
-                                if (!Environment.isExternalStorageManager()){
-                                    Toast.makeText(getApplicationContext(), "Une permission est requise pour cette fonctionalitée", Toast.LENGTH_LONG).show();
-                                    Intent getpermission = new Intent();
-                                    getpermission.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-                                    startActivity(getpermission);
-                                }
-                            }
-                            /** Récupération et enrengistrement des images se trouvant déja en favorites (dans la base de données) */
-                            MyDatabase myDatabase = new MyDatabase(getApplicationContext());
-                            ArrayList<Image> ImageList = myDatabase.readData(true);
-                            for (Image i : ImageList){
-                                i.saveimage(getApplicationContext(),prefs.getString("emplacement",""));
+        /* Définit les actions à réaliser au moment du changement des préférences */
+        prefs.registerOnSharedPreferenceChangeListener((sharedPreferences, key) -> {
+            switch (key){
+                case "theme":
+                    /*
+                     * Change le thème
+                     * Alerte l'utlisateur qu'un redémrrage sera peut-etre necessaire
+                     * Re-créaction de l'activité afin de changer le thème */
+                    GetThemeFromSharedPreference();
+                    Toast.makeText(getApplicationContext(), "Si le thème ne change pas, redémarrer l'application", Toast.LENGTH_LONG).show();
+                    recreate();
+                    break;
+                case "telecharger":
+                    /* Si l'enrengistrement devient autorisé lors du changement */
+                    /*
+                     * Alerte l'utlisateur qu'une permission supplémentaire (MANAGE_ALL_FILES) est requise
+                     * Commence une nouvelle activité permettant à l'utlisateur d'autoriser l'enrengistrement des images
+                     * */
+                    if((boolean) sharedPreferences.getAll().get(key)){
+                        if (Build.VERSION.SDK_INT >= 30){
+                            if (!Environment.isExternalStorageManager()){
+                                Toast.makeText(getApplicationContext(), "Une permission est requise pour cette fonctionalitée", Toast.LENGTH_LONG).show();
+                                Intent getpermission = new Intent();
+                                getpermission.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                                startActivity(getpermission);
                             }
                         }
-                        break;
-                    case "emplacement":
-                        /** Renommage du dossier comprenant les images téléchargées
-                         * dossierDeStockage = ancier nom du dossier
-                         * clé "emplacement" des préférences = nouveau nom du dossier
-                         * */
-                        String root = Environment.getExternalStorageDirectory().toString();
-                        File myDir = new File(root + dossierDeStockage );
-                        if (!myDir.exists()) {
-                            myDir.mkdirs();
+                        /* Récupération et enrengistrement des images se trouvant déja en favorites (dans la base de données) */
+                        MyDatabase myDatabase = new MyDatabase(getApplicationContext());
+                        ArrayList<Image> ImageList = myDatabase.readData(true);
+                        for (Image i : ImageList){
+                            i.saveimage(getApplicationContext(),prefs.getString("emplacement",""));
                         }
-                        myDir.renameTo(new File(root + sharedPreferences.getString("emplacement","")));
-                        /** Actualisation de la variable aprés le changement */
-                        dossierDeStockage = sharedPreferences.getString("emplacement","");
+                    }
+                    break;
+                case "emplacement":
+                    /* Renommage du dossier comprenant les images téléchargées
+                     * dossierDeStockage = ancier nom du dossier
+                     * clé "emplacement" des préférences = nouveau nom du dossier
+                     * */
+                    String root = Environment.getExternalStorageDirectory().toString();
+                    File myDir = new File(root + dossierDeStockage );
+                    if (!myDir.exists()) {
+                        if(!myDir.mkdirs()){
+                            Log.d(TAG,"Erreur lors de la création du dossier");
+                            break;
+                        }
+                    }
+                    if(!myDir.renameTo(new File(root + sharedPreferences.getString("emplacement","")))){
+                        Log.d(TAG,"Erreur lors du changement de nom du dossier");
                         break;
-                }
+                    }
+                    /* Actualisation de la variable aprés le changement */
+                    dossierDeStockage = sharedPreferences.getString("emplacement","");
+                    break;
             }
         });
     }
@@ -150,9 +156,10 @@ public class MainActivity extends AppCompatActivity {
      * @param item MenuItem
      * @return true si l'option existe
      */
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        /** Indique vers quel fragment se diriger selon l'item de la toolbar séléctioné */
+        /* Indique vers quel fragment se diriger selon l'item de la toolbar séléctioné */
         switch (item.getItemId()) {
             case R.id.fragR:
                 getSupportFragmentManager().beginTransaction()
