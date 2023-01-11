42dd945f44c0992e47d16dd53bc4525405fe8237
Lili493118 <aoustinalyson@gmail.com>
2023-01-11 11:00:34 +0100

    AdaptateurListImage

--- app/src/main/java/com/example/projet_aoustin/AdaptateurListImage.java
+++ app/src/main/java/com/example/projet_aoustin/AdaptateurListImage.java
@@ -1,8 +1,7 @@
 package com.example.projet_aoustin;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,25 +13,45 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * Classe Adaptateur permettant d'afficher une listView complexe composé d'image et de son titre
+ */
 public class AdaptateurListImage extends ArrayAdapter<Image> {
+    /**
+     * Constructeur
+     * @param context @NonNull Context
+     * @param imgs  ArrayList<Image> List contenant les Images (classe contenant une images et un titre) qui devront se trouver dans la liste
+     */
     public AdaptateurListImage(@NonNull Context context, ArrayList<Image> imgs) {
+        /*L'adapteur se basera sur le layout element_image_list pour afficher chaque item de la liste imgs
+        * La listView est composée de plusieurs element_image_list les uns en dessous des autres*/
         super(context, R.layout.element_image_list,imgs);
     }
 
+    /**
+     * Fonction affichant les éléments de notre listView
+     * @param position int position de l'element textView
+     * @param convertView View
+     * @param parent ViewGroup
+     * @return View convertView
+     */
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        /*Récupération de l'Image se trouvant à la position "position"*/
         Image img = getItem(position);
+
         if(convertView == null){
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.element_image_list,parent,false);
         }
 
-        // Ajout de notre image à la vue de notre liste
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageViewElement);
+        /* Mapping des élements de notre Image aux élements de R.layout.element_image_list
+        * Set du bitmap de l'Image
+        * Set du titre de l'Image*/
+        ImageView imageView = convertView.findViewById(R.id.imageViewElement);
         imageView.setImageBitmap(img.getBitmap());
 
-        TextView TextView = (android.widget.TextView) convertView.findViewById(R.id.titleimage);
+        TextView TextView = convertView.findViewById(R.id.titleimage);
         TextView.setText(img.getTitre());
 
         return convertView;
