2a84d76cbd91d96e93428b0e534965245ba3e0d8
Lili493118 <aoustinalyson@gmail.com>
2023-01-11 15:47:03 +0100

    onupgrade

--- app/src/main/java/com/example/projet_aoustin/Service_Image.java
+++ app/src/main/java/com/example/projet_aoustin/Service_Image.java
@@ -3,17 +3,12 @@ package com.example.projet_aoustin;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import android.app.Service;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Binder;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.IBinder;
-import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -21,107 +16,129 @@ import androidx.annotation.Nullable;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
+
+/**
+ * Classe Service qui permet de récupérer des images et leurs informations à partir d'un mot clé
+ */
 public class Service_Image extends Service {
-    private static String TAG = "grenouille";
+    /** TAG servant au log  */
+    private static final String TAG = "Test";
+    /** Listener qui permet le transfert d'information vers le fragment grace à la méthode update*/
     private Listener_Service_Image listener_service_image;
+    /** Binder permttant de récupérer le service apres son démarrage dans le fragment (communication inter-processus)*/
     private final IBinder binder = new MonBinder();
-    public String test = "debut";
 
+    /**
+     * Binder personnalisé
+     * permet de récuprer le service à travers le Binder
+     */
     public class MonBinder extends Binder {
+        /**
+         * Retourne le service
+         * @return Service_Image this
+         */
         Service_Image getService() {
             return Service_Image.this;
         }
     }
 
+    /**
+     * Méthode appelée lors de la création du service
+     */
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(TAG, "OnCreate");
+        Log.d(TAG, "Création du service");
     }
 
+    /**
+     * Methode appelée lors du démarrage du service
+     * @param intent Intent
+     * @param flags int
+     * @param startId int
+     * @return super.onStartCommand(intent, flags, startId);
+     */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, "onStartCommand");
-        this.test = "fin";
+        Log.d(TAG, "Démarrage du service");
+        /*Récupération du mot clé se trouvant dans les extras */
         Bundle getExtra = intent.getExtras();
-        String keyword = new String(getExtra.getString("recherche"));
+        String keyword = getExtra.getString("recherche");
 
-        new Thread(new Runnable() {
-            public void run() {
-                int progress = 0;
+        /*Création d'un Thread récupérant les images en background*/
+        new Thread(() -> {
+            /*Initialisation des variables */
+            int progress = 0;
+            URL url ;
 
-                URL url = null;
-                try {
-                    url = new URL("https://www.flickr.com/services/feeds/photos_public.gne?tags=" + keyword + "&format=json");
-                    StringBuilder builder = new StringBuilder();
+            try {
+                /*Récupération de l'URL à partir de l'url */
+                url = new URL("https://www.flickr.com/services/feeds/photos_public.gne?tags=" + keyword + "&format=json");
+                StringBuilder resultStringBuilder = new StringBuilder();
 
-                    try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream(), UTF_8))) {
-                        String str;
-                        builder.append(bufferedReader.readLine().substring(15));
-                        while ((str = bufferedReader.readLine()) != null) {
-                            builder.append(str);
-                        }
-                        String jsonStr = builder.toString();
-                        JSONObject jsonObject = new JSONObject(jsonStr);
-                        JSONArray jsonArray = jsonObject.getJSONArray("items");
-                        //Image image = new Image.Builder().auteur("test").build();
-                        ArrayList<Image> ImageList = new ArrayList<>();
+                /*Récupération de la réponse dans resultStringBuilder  */
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream(), UTF_8))) {
+                    /*Initialisation de variables*/
+                    String str;
+                    ArrayList<Image> ImageList = new ArrayList<>();
 
-                        for (int i = 0; i < jsonObject.getJSONArray("items").length(); i++) {
-                            Log.d("reponse", jsonArray.getJSONObject(i).get("title").toString());
-                            Bitmap bitmap = null;
-                            listener_service_image.progress(progress++);
-                            try {
-                                bitmap = BitmapFactory.decodeStream((InputStream) new URL(
-                                        jsonArray.getJSONObject(i).getJSONObject("media").get("m").toString()).getContent());
-
-                                ImageList.add(new Image.Builder()
-                                        .bitmap(bitmap)
-                                        .titre(jsonArray.getJSONObject(i).get("title").toString())
-                                        .date(jsonArray.getJSONObject(i).get("date_taken").toString())
-                                        .auteur(jsonArray.getJSONObject(i).get("author").toString())
-                                        .description(jsonArray.getJSONObject(i).get("description").toString())
-                                        .lien(jsonArray.getJSONObject(i).get("link").toString())
-                                        .build()
-                                );
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        listener_service_image.update(ImageList);
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                    /*Réduction de la réponse afin de supprimer le début qui n'est pas sous la forme JSON*/
+                    resultStringBuilder.append(bufferedReader.readLine().substring(15));
+                    while ((str = bufferedReader.readLine()) != null) {
+                        resultStringBuilder.append(str);
                     }
+                    /*Convertir StringBuiler to String*/
+                    String resultString = resultStringBuilder.toString();
+                    /*Convertir String to JSONObject afin de rendre le parcours plus simple */
+                    JSONObject resultJsonObj = new JSONObject(resultString);
+                    /*Parcours retournant le tableau d'items*/
+                    JSONArray resultArray = resultJsonObj.getJSONArray("items");
 
-                } catch (MalformedURLException e) {
+                    /*Pour chaque élement du résultat */
+                    for (int i = 0; i < resultArray.length(); i++) {
+                        Log.d(TAG, resultArray.getJSONObject(i).get("title").toString());
+                        /*Initialisation */
+                        Bitmap bitmap ;
+                        /*Augmentation du progres et appel de la fonction mettant à jour la barre de progression*/
+                        listener_service_image.progress(progress++);
+                        try {
+                            /*Génération du Bitmap en fonction de l'URL*/
+                            bitmap = BitmapFactory.decodeStream((InputStream) new URL(
+                                    resultArray.getJSONObject(i).getJSONObject("media").get("m").toString()).getContent());
+
+                            /*Construction et ajout d'une Image */
+                            ImageList.add(new Image.Builder()
+                                    .bitmap(bitmap)
+                                    .titre(resultArray.getJSONObject(i).get("title").toString())
+                                    .date(resultArray.getJSONObject(i).get("date_taken").toString())
+                                    .auteur(resultArray.getJSONObject(i).get("author").toString())
+                                    .description(resultArray.getJSONObject(i).get("description").toString())
+                                    .lien(resultArray.getJSONObject(i).get("link").toString())
+                                    .build()
+                            );
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    /*Appel de la méthode update qui previent le fragment, celui-ci affiche le résultat */
+                    listener_service_image.update(ImageList);
+
+                } catch (IOException | JSONException e) {
                     e.printStackTrace();
                 }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
             }
         }).start();
 
@@ -129,12 +146,21 @@ public class Service_Image extends Service {
     }
 
 
+    /**
+     * Méthode appelé au moment du bind
+     * @param intent Intent
+     * @return IBinder
+     */
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
     }
 
+    /**
+     * Permet d'enrengistrer un listener à ce service
+     * @param monListener Listener_Service_Image
+     */
     public void setMonListener(Listener_Service_Image monListener) {
         this.listener_service_image = monListener;
     }
